7e6df8cbfc43756e802ee20280106bdd43e8d277
Ian Oliveira <ianprogrammer@gmail.com>
2019-12-26 15:25:09 -0300

    adding a frontend

--- app.js
+++ app.js
@@ -10,20 +10,30 @@ const graphQLBookingResolvers = require('./graphql/resolvers/booking')
 const isAuth = require('./middleware/is-auth')
 
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST,GET, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers","Content-Type, Authorization")
+
+    if(req.method === 'OPTIONS'){
+        return res.sendStatus(200)
+    }
+    next()
+})
 app.use(isAuth)
 
 app.use('/graphql', graphQLHttp({
     schema: graphQLSchema,
-    rootValue: {...graphQLEventResolvers,...graphQLUserResolvers, ... graphQLBookingResolvers},
+    rootValue: { ...graphQLEventResolvers, ...graphQLUserResolvers, ...graphQLBookingResolvers },
     graphiql: true
 
 }))
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${
     process.env.MONGO_PASSWORD
-    }@cluster0-g7qoz.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,{ useUnifiedTopology: true,useNewUrlParser: true },
+    }@cluster0-g7qoz.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`, { useUnifiedTopology: true, useNewUrlParser: true },
 ).then(() => {
-    app.listen(3000)
+    app.listen(8000)
 }).catch(err => {
     console.error(err)
 })
---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+import AuthPage from './pages/Auth';
+import EventsPage from './pages/Events';
+import BookingsPage from './pages/Bookings';
+import MainNavigation from './components/Navigation/MainNavigation'
+
+
+function App() {
+  return (
+
+    <BrowserRouter>
+      <React.Fragment>
+        <MainNavigation />
+        <main className="main-content">
+          <Switch>
+            <Redirect from="/" to="/auth" exact />
+            <Route path="/auth" component={AuthPage} />
+            <Route path="/events" component={EventsPage} />
+            <Route path="/bookings" component={BookingsPage} />
+          </Switch>
+        </main>
+      </React.Fragment>
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
